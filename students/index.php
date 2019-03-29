f8a23c1e066ce56dd39a215d88772f88b6a5c138
dan-97 <danny.c.ntwari@gmail.com>
2019-03-29 19:25:40 +0200

    Cosmetic edits
    Change the titles of login pages (index.php) for student, teacher and admin portal

--- students/index.php
+++ students/index.php
@@ -42,7 +42,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Students Index</title>
+<title>Students Login | SMS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Augment Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
